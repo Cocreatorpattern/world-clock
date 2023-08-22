e7d9a76083963563d8bc0ade96c8cbf69a9c0f2e
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-08-22 19:37:21 +0100

    Added Java script

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+
+  let ParisElement = document.querySelector("#Paris");
+  let ParisDateElement = ParisElement.querySelector(".date");
+  let ParisTimeElement = ParisElement.querySelector(".time");
+  let ParisTime = moment().tz("Europe/Paris");
+
+  ParisDateElement.innerHTML = ParisTime.format("MMMM Do YYYY");
+  ParisTimeElement.innerHTML = ParisTime.format("h:mm:ss[<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
