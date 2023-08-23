7b9be4a18fe32b12144c8d61ac45811233d55792
Cocreatorpattern <136176709+Cocreatorpattern@users.noreply.github.com>
2023-08-23 10:09:08 +0100

    added moment to cities

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function updateTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
   let losAngelesDateElement = losAngelesElement.querySelector(".date");
   let losAngelesTimeElement = losAngelesElement.querySelector(".time");
   let losAngelesTime = moment().tz("America/Los_Angeles");
@@ -8,14 +9,41 @@ function updateTime() {
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
     "h:mm:ss[<small>]A[</small>]"
   );
-
+}
   let ParisElement = document.querySelector("#Paris");
+  if (ParisElement) {
   let ParisDateElement = ParisElement.querySelector(".date");
   let ParisTimeElement = ParisElement.querySelector(".time");
   let ParisTime = moment().tz("Europe/Paris");
 
   ParisDateElement.innerHTML = ParisTime.format("MMMM Do YYYY");
-  ParisTimeElement.innerHTML = ParisTime.format("h:mm:ss[<small>]A[</small>]");
+  ParisTimeElement.innerHTML = ParisTime.format("h:mm:ss[<small>]A[</small>]"
+  );
+}
+ }
+
+ function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName =cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+ <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+  
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+
+let citiesSelectElement=document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change",updateCity);
